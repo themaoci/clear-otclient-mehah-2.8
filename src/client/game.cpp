2c5fd21c3dab09ca60d78a321dbe30d12c05c136
TheMaoci <themaoci@gmail.com>
2023-03-22 17:04:06 +0100

    NPC Trade - Missed this one

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -430,7 +430,7 @@ void Game::processOpenOutfitWindow(const Outfit& currentOutfit, const std::vecto
     g_lua.callGlobalField("g_game", "onOpenOutfitWindow", virtualOutfitCreature, outfitList, virtualMountCreature, mountList);
 }
 
-void Game::processOpenNpcTrade(const std::vector<std::tuple<ItemPtr, std::string, int, int, int> >& items)
+void Game::processOpenNpcTrade(const std::vector<std::tuple<ItemPtr, std::string, int, int, int, int> >& items)
 {
     g_lua.callGlobalField("g_game", "onOpenNpcTrade", items);
 }
